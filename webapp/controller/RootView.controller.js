1b65aedf19b766ecac1e8d2c1fbe5d9cebc266bb
Shen Jing <jing.shen@mediatek.com>
2019-07-25 07:22:50 +0000

    V2

--- webapp/controller/RootView.controller.js
+++ webapp/controller/RootView.controller.js
@@ -1,11 +1,43 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/odata/v2/ODataModel"
+], function (Controller, ODataModel) {
 	"use strict";
 
 	return Controller.extend("com.mtk.jingUI5_smarttable.controller.RootView", {
 		onInit: function () {
+			//var oModel, oView;
+			//oModel = new ODataModel("/sap/opu/odata/sap/Z_JING_C_SALESORDER_TX_CDS/", true);
+			//oModel.setCountSupported(false);
+			//oView = this.getView();
+			//oView.setModel(oModel);
+		},
+		onBeforeExport: function (oEvt) {
+			var mExcelSettings = oEvt.getParameter("exportSettings");
+			// GW export
+			if (mExcelSettings.url) {
+				return;
+			}
+			// UI5 Client Export
+			mExcelSettings.fileName = "SmartTable-Export"; // example to modify fileName
 
+			// Sample customization
+			if (mExcelSettings.workbook && mExcelSettings.workbook.columns) {
+				mExcelSettings.workbook.columns.some(function (oColumnConfiguration) {
+					// Customize output for GrossAmount column to match the text on the UI, instead of showing the currency
+					if (oColumnConfiguration.property === "GrossAmountX") {
+						oColumnConfiguration.unitProperty = "CurrencyCode"; // Decimal handling
+						oColumnConfiguration.textAlign = "Right";
+						oColumnConfiguration.displayUnit = false;
+						oColumnConfiguration.type = "currency"; // Change type of column
+						oColumnConfiguration.width = 12; // Set desired width
+						return true;
+					}
+				});
+			}
+
+			// Disable Worker if Mockserver is used in explored
+			mExcelSettings.worker = true;
 		}
 	});
 });
